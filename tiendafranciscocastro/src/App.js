00a3c20f95945498ce5d4ae0a45fadbacdd46ee5
Francisco <Francisco@tests-MBP.fibertel.com.ar>
2022-08-20 16:19:17 -0300

    Primer repositorio con MacPro 20.08

--- tiendafranciscocastro/src/App.js
+++ tiendafranciscocastro/src/App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
 
     
-      <h1>¡HOLA MUNDO!</h1>
+      // <><><><>
+      // <>
 
+      <NavBar/>
+/*         
+
+        <h1>¡HOLA MUNDO!</h1>
+        <Saludos name='Francisco' lastname='Castro' /></>
+      <Saludos name='Marina' lastname='Luissi' /></>
+
+      <li>VER</li></>
+      <li>VER2</li></>
+      <li>VER3</li></> */
+            
+        
+
+
+
+/* 
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
@@ -14,15 +32,15 @@ function App() {
     //       Edit <code>src/App.js</code> and save to reload.
     //     </p>
     //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div> */
   );
 }
 
---
+++ tiendafranciscocastro/src/components/NavBar.js
@@ -0,0 +1,28 @@
+// import React from 'react'
+
+// const Saludos = ({name, lastname, children}) => {
+//   return (
+//     <>
+//      <h1>Saludos a {name} {lastname}</h1>
+//      {children}
+//     </>
+   
+//   )
+// }
+
+//export default Saludos//
+
+
+
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+      <li>Quienes somos</li>
+      <li>Quienes somos</li>
+    </div>
+  )
+}
+
+export default NavBar
