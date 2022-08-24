2c3f4341a0b1ddcd52460d3f67d972bded772f3e
FranCastro <franciscocast@gmail.com>
2022-08-24 16:36:55 -0300

    Version 24.08 solo con Tailwind instalado

--- tiendafranciscocastro/src/App.js
+++ tiendafranciscocastro/src/App.js
@@ -11,6 +11,10 @@ function App() {
     
     <section className="Productos">
 
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
         <h2>Quienes somos</h2>
           <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a ligula malesuada, fringilla nulla et, pellentesque mi. Vivamus ultrices, quam nec egestas dictum, est ex porta risus, eget sodales ex massa consequat ante. Curabitur eget dolor eget diam volutpat commodo laoreet eu enim. Donec sed erat nibh. Morbi sit amet sapien at odio vulputate pellentesque. Praesent tincidunt erat nunc. Donec ac porttitor orci. Sed sit amet ligula sit amet orci interdum sodales. Donec at enim sit amet sapien suscipit mollis. Phasellus id lacus sed metus scelerisque fringilla. Donec vel arcu viverra, pretium nunc a, aliquet eros. Quisque congue a dui quis porta.
